5ea37eeb36a48841caf9fe03f4d9ad17aed9c33a
Henrique Varella Ehrenfried <hvehrenfried@inf.ufpr.br>
2017-04-23 17:16:18 -0300

    Add Script to run multiple times

---
+++ Spark/multipleTimes.rb
@@ -0,0 +1,21 @@
+numVezes = ARGV[0]
+
+directory_name = "Resultados"
+Dir.mkdir(directory_name) unless File.exists?(directory_name)
+files = [
+            "Obras_por_estágio.svg",
+            "Obras_por_unidades_da_federação.svg",
+            "Obras_por_unidades_da_federação_e_estágio.svg",
+            "Porcentagem_de_erro_da_predição.svg",
+            "results.txt"
+        ]
+
+(0..numVezes).to_a.each do |run|
+    directory_name = "Rodada #{run}"
+    Dir.mkdir(directory_name) unless File.exists?(directory_name)
+    system("python bd-tp1.py")
+    files.each do |f|
+        system("mv #{f} #{directory_name}")
+    end
+    system("mv #{directory_name} Resultados")
+end
